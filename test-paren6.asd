6bd0fdee517964f3aa6d7b48c32b8dccb6cba7dc
bnmcgn@gmail.com <bnmcgn@gmail.com>
2019-09-21 08:20:02 -0700

    Added some tests

---
+++ test-paren6.asd
@@ -0,0 +1,15 @@
+;;;; test-paren6.asd
+
+(asdf:defsystem #:test-paren6
+  :description "Tests for Paren6"
+  :author "Ben McGunigle <bnmcgn@gmail.com>"
+  :license "Apache License, version 2.0"
+  :depends-on (#:parenscript
+               #:paren6
+               #:external-program) 
+  :serial t
+  :components ((:module :t
+                        :serial t
+                        :components ((:file "test-paren6")))))
+
+
---
+++ test/test-paren6.lisp
@@ -0,0 +1,93 @@
+
+(in-package :cl-user)
+(defpackage test-paren6
+  (:use #:cl #:parenscript #:paren6))
+
+(in-package :test-paren6)
+
+
+(defun tests ()
+  (ps
+    (defvar chai (require "chai"))
+    (defvar expect (@ chai expect))
+
+    (describe
+     "list6"
+     (lambda ()
+       (it "Should return an array equal to [1, 2, 3, 4, 5]"
+           (lambda ()
+             (chain
+              (expect (list6 1 :... (list 2 3 4) 5))
+              ;; or: to have ordered (members (list 1 2 3 4 5))
+              to be (an "array") that deep (equals (list 1 2 3 4 5)))))))
+    (describe
+     "create6"
+     (lambda ()
+       (defvar arr (create :a 1 :b 2))
+       (defvar a 5)
+       (defvar c 10)
+       (defvar item (create6
+                     a c :... arr (:b 3)
+                     (set thing (x) (setf (@ this _thing) x))
+                     (get thing () (@ this _thing))
+                     (defun action (x) x)))
+       (it "Should allow same name setting"
+           (lambda ()
+             (chain
+              (expect (@ item c)) to (equal 10))))
+       (it "Should shadow values in the correct order"
+           (lambda ()
+             (chain
+              (expect (+ (@ item a) (@ item b)))
+              to (equal 4))))
+       (it "Should allow creation of setters and getters"
+           (lambda ()
+             (setf (@ item thing) "asdf")
+             (chain (expect (@ item thing))
+                    to (equal "asdf"))))
+       (it "Should allow the creation of object methods"
+           (lambda ()
+             (chain (expect (@ item action)) to be (a "function"))))))
+    (describe
+     "=>"
+     (lambda ()
+       (it "Shouldn't have its own 'this'"
+           (lambda ()
+             (setf (@ this y) 2)
+             (chain
+              (expect (funcall (=> x (+ x (@ this y))) 3))
+              to be (equal 5))))))
+    ))
+
+  #|
+
+list6
+  create6
+  defclass6
+  =>
+  export
+  import
+  export-default
+  const6
+
+|#
+
+(defun run-tests ()
+  (with-open-file (s (asdf:system-relative-pathname 'test-paren6 "test/tests.js")
+                     :direction :output :if-exists :supersede :if-does-not-exist :create)
+    (write-string (tests) s))
+  (uiop:with-current-directory ((asdf:system-source-directory 'test-paren6))
+    (print
+     (with-output-to-string (out)
+       (external-program:run
+        "test/node_modules/mocha/bin/mocha" nil :output out :error :output)))))
+
+#|
+(defun run-tests ()
+  (uiop:with-current-directory ((asdf:system-relative-pathname 'test-paren6 "t/"))
+    (with-input-from-string (in (print (tests)))
+      (print
+       (with-output-to-string (out)
+         (external-program:run
+          "node" (list "-r" "mocha" "-r" "chai") :input in :output out :error :output))))))
+|#
